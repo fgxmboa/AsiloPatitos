7d54d9dacbde6f48102e396ec94ab6262dacf5d4
Francisco Gamboa <franciscogamboa03@hotmail.com>
2025-10-29 14:00:43 -0600

    feat(domain): agrega entidades Empleado y Paciente

---
+++ AsiloPatitos.Domain/Entities/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsiloPatitos.Domain.Entities
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Cedula { get; set; } = null!;
+        public DateTime FechaIngreso { get; set; }
+        public string Departamento { get; set; } = null!;
+        public string Perfil { get; set; } = null!;
+    }
+
+}
---
+++ AsiloPatitos.Domain/Entities/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsiloPatitos.Domain.Entities
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Cedula { get; set; } = null!;
+        public DateTime FechaIngreso { get; set; }
+        public string Medicamentos { get; set; } = null!;
+        public string CuidadosEspeciales { get; set; } = null!;
+        public string NivelAsistencia { get; set; } = null!;
+        public string PaqueteAdicional { get; set; } = null!;
+    }
+}
