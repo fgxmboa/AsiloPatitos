074505b843b199dc9a9f52254ec3f5569b4534e4
aliwh <aliwh@ALIWU-LAPTOP>
2025-11-25 11:28:11 -0600

    #

---
+++ AsiloPatitos.Infrastructure/Migrations/20251125171331_AddConexion.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsiloPatitos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConexion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Observaciones",
+                table: "PacienteTratamientos",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Observaciones",
+                table: "PacienteTratamientos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+        }
+    }
+}
