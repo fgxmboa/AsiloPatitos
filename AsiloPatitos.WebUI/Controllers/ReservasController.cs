04580d5d68495f5705b18c74cd3376e9241a6bb8
Francisco <franciscogamboa03@hotmail.com>
2025-11-25 05:07:37 -0600

    Validaciones de Pago y Reserva

--- AsiloPatitos.WebUI/Controllers/ReservasController.cs
+++ AsiloPatitos.WebUI/Controllers/ReservasController.cs
@@ -22,26 +22,25 @@ namespace AsiloPatitos.WebUI.Controllers
         // GET: Reservas
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Reservas.Include(r => r.Habitacion).Include(r => r.Paciente);
-            return View(await applicationDbContext.ToListAsync());
+            var reservas = await _context.Reservas
+                .Include(r => r.Paciente)
+                .Include(r => r.Habitacion)
+                .ToListAsync();
+
+            return View(reservas);
         }
 
         // GET: Reservas/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var reserva = await _context.Reservas
-                .Include(r => r.Habitacion)
                 .Include(r => r.Paciente)
+                .Include(r => r.Habitacion)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (reserva == null)
-            {
-                return NotFound();
-            }
+
+            if (reserva == null) return NotFound();
 
             return View(reserva);
         }
@@ -49,44 +48,59 @@ namespace AsiloPatitos.WebUI.Controllers
         // GET: Reservas/Create
         public IActionResult Create()
         {
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Nombre");
             ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero");
-            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Cedula");
             return View();
         }
+
 
         // POST: Reservas/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,PacienteId,HabitacionId,FechaIngreso,FechaSalida,Total,Estado")] Reserva reserva)
+        public async Task<IActionResult> Create(Reserva reserva)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Por favor complete todos los campos requeridos.";
+
+                ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Nombre", reserva.PacienteId);
+                ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
+
+                return View(reserva);
+            }
+
+            try
             {
                 _context.Add(reserva);
                 await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "Reserva creada correctamente.";
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
-            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Cedula", reserva.PacienteId);
-            return View(reserva);
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Ocurrió un error al guardar la reserva: " + ex.Message;
+
+                ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Nombre", reserva.PacienteId);
+                ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
+
+                return View(reserva);
+            }
         }
 
         // GET: Reservas/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var reserva = await _context.Reservas.FindAsync(id);
-            if (reserva == null)
-            {
-                return NotFound();
-            }
+            if (reserva == null) return NotFound();
+
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Nombre", reserva.PacienteId);
             ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
-            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Cedula", reserva.PacienteId);
+
             return View(reserva);
         }
 
@@ -95,54 +109,54 @@ namespace AsiloPatitos.WebUI.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PacienteId,HabitacionId,FechaIngreso,FechaSalida,Total,Estado")] Reserva reserva)
+        public async Task<IActionResult> Edit(int id, Reserva reserva)
         {
-            if (id != reserva.Id)
+            if (id != reserva.Id) return NotFound();
+
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                TempData["ErrorMessage"] = "Por favor complete todos los campos requeridos.";
+
+                ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Nombre", reserva.PacienteId);
+                ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
+
+                return View(reserva);
             }
 
-            if (ModelState.IsValid)
+            try
             {
-                try
-                {
-                    _context.Update(reserva);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReservaExists(reserva.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                _context.Update(reserva);
+                await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = "Reserva actualizada correctamente.";
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["HabitacionId"] = new SelectList(_context.Habitaciones, "Id", "Numero", reserva.HabitacionId);
-            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Cedula", reserva.PacienteId);
-            return View(reserva);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservaExists(reserva.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Error de concurrencia al actualizar.";
+                    return View(reserva);
+                }
+            }
         }
+
 
         // GET: Reservas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var reserva = await _context.Reservas
-                .Include(r => r.Habitacion)
                 .Include(r => r.Paciente)
+                .Include(r => r.Habitacion)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (reserva == null)
-            {
-                return NotFound();
-            }
+
+            if (reserva == null) return NotFound();
 
             return View(reserva);
         }
@@ -156,9 +170,9 @@ namespace AsiloPatitos.WebUI.Controllers
             if (reserva != null)
             {
                 _context.Reservas.Remove(reserva);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Reserva eliminada correctamente.";
             }
-
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
