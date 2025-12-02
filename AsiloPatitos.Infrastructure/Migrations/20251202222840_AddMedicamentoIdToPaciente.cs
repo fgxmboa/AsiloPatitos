1e37d4600903722f45358de686a1f3ed62e7f177
Francisco <franciscogamboa03@hotmail.com>
2025-12-02 17:37:02 -0600

    Optimizacion de la vista de Pacientes

---
+++ AsiloPatitos.Infrastructure/Migrations/20251202222840_AddMedicamentoIdToPaciente.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsiloPatitos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMedicamentoIdToPaciente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Habitaciones_PacienteId",
+                table: "Habitaciones");
+
+            migrationBuilder.DropColumn(
+                name: "HabitacionId",
+                table: "Pacientes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MedicamentoId",
+                table: "Pacientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_MedicamentoId",
+                table: "Pacientes",
+                column: "MedicamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habitaciones_PacienteId",
+                table: "Habitaciones",
+                column: "PacienteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pacientes_Medicamentos_MedicamentoId",
+                table: "Pacientes",
+                column: "MedicamentoId",
+                principalTable: "Medicamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pacientes_Medicamentos_MedicamentoId",
+                table: "Pacientes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pacientes_MedicamentoId",
+                table: "Pacientes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Habitaciones_PacienteId",
+                table: "Habitaciones");
+
+            migrationBuilder.DropColumn(
+                name: "MedicamentoId",
+                table: "Pacientes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "HabitacionId",
+                table: "Pacientes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habitaciones_PacienteId",
+                table: "Habitaciones",
+                column: "PacienteId",
+                unique: true,
+                filter: "[PacienteId] IS NOT NULL");
+        }
+    }
+}
